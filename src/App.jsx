7b9ac403a1a200e4045cf07956052085f3cb20b2
Vansh Tomar <tomarvansh90@gmail.com>
2025-08-23 09:42:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,30 @@ import Products from './pages/Products'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Cart from './pages/Cart'
+import SignupPage from './pages/SignupPage'
 import Navbar from './components/Navbar'
-import axios from 'axios'
 import Footer from './components/Footer'
 import SingleProduct from './pages/SingleProduct'
 import CategoryProduct from './pages/CategoryProduct'
-import { useCart } from './context/CartContext'
 import ProtectedRoute from './components/ProtectedRoute'
-
+import axios from 'axios'
 
 const App = () => {
   const [location, setLocation] = useState()
   const [openDropdown, setOpenDropdown] = useState(false)
-  const { cartItem, setCartItem } = useCart()
 
   const getLocation = async () => {
     navigator.geolocation.getCurrentPosition(async pos => {
       const { latitude, longitude } = pos.coords
-      // console.log(latitude, longitude);
-
       const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`
       try {
         const location = await axios.get(url)
         const exactLocation = location.data.address
         setLocation(exactLocation)
         setOpenDropdown(false)
-        // console.log(exactLocation);
-
       } catch (error) {
-        console.log(error);
-
+        console.log(error)
       }
-
     })
   }
 
@@ -44,32 +36,30 @@ const App = () => {
     getLocation()
   }, [])
 
-  //Load cart from local storage on initial render
-  useEffect(() => {
-    const storedCart = localStorage.getItem('cartItem')
-    if(storedCart){
-      setCartItem(JSON.parse(storedCart))
-    }
-  }, []);
-
-  //save cart to local storage whenever it changes
-  useEffect(() => {
-    localStorage.setItem('cartItem', JSON.stringify(cartItem))
-  }, [cartItem])
-
   return (
     <BrowserRouter>
-      <Navbar location={location} getLocation={getLocation} openDropdown={openDropdown} setOpenDropdown={setOpenDropdown} />
+      <Navbar 
+        location={location} 
+        getLocation={getLocation} 
+        openDropdown={openDropdown} 
+        setOpenDropdown={setOpenDropdown} 
+      />
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/products' element={<Products />}></Route>
-        <Route path='/products/:id' element={<SingleProduct />}></Route>
-        <Route path='/category/:category' element={<CategoryProduct />}></Route>
-        <Route path='/about' element={<About />}></Route>
-        <Route path='/contact' element={<Contact />}></Route>
-        <Route path='/cart' element={<ProtectedRoute>
-          <Cart location={location} getLocation={getLocation} />
-        </ProtectedRoute>}></Route>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/products/:id' element={<SingleProduct />} />
+        <Route path='/category/:category' element={<CategoryProduct />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route 
+          path='/cart' 
+          element={
+            <ProtectedRoute>
+              <Cart location={location} getLocation={getLocation} />
+            </ProtectedRoute>
+          } 
+        />
       </Routes>
       <Footer />
     </BrowserRouter>
